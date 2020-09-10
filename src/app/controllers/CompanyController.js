347ab00c9d58213c793278d79ac334068c26ef02
unknown <jeffynho32@gmail.com>
2020-09-10 10:40:15 -0300

    feat(controllers/CompanyController): Definindo comtroles da tabela companies

--- src/app/controllers/CompanyController.js
+++ src/app/controllers/CompanyController.js
@@ -2,36 +2,6 @@ import * as Yup from 'yup';
 import Company from '../models/Company';
 
 class CompanyControllers {
-  async store(req, res) {
-    const schema = Yup.object().shape({
-      description: Yup.string().required(),
-      email: Yup.string().email().required(),
-      password: Yup.string().required().min(6),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Dados inválidos' });
-    }
-
-    const { email } = req.body;
-
-    const checkEmail = await Company.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (checkEmail) {
-      return res
-        .status(400)
-        .json({ error: 'Este email já está sendo usado em nosso sistema' });
-    }
-
-    const company = await Company.create(req.body);
-
-    return res.json(company);
-  }
-
   async update(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string(),
