9f943db6851979d6725f69ffdb24e793e2731bc3
unknown <jeffynho32@gmail.com>
2020-09-22 11:02:21 -0300

    env: Criando tabela customers na base de dados

---
+++ src/app/models/Customer.js
@@ -0,0 +1,37 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Customer extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        type: Sequelize.INTEGER,
+        name: Sequelize.STRING,
+        cpf: Sequelize.STRING,
+        cnpj: Sequelize.STRING,
+        email: Sequelize.STRING,
+        telephone: Sequelize.STRING,
+        cell_phone: Sequelize.STRING,
+        street: Sequelize.STRING,
+        number: Sequelize.STRING,
+        neighborhood: Sequelize.STRING,
+        complement: Sequelize.STRING,
+        city: Sequelize.STRING,
+        state: Sequelize.STRING,
+        active: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+        tableName: 'customers',
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Company, { foreignKey: 'company_id', as: 'company' });
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+}
+
+export default Customer;
