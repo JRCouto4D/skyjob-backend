0d740a34391e6c31328c595b978cce9a305d80cc
unknown <jeffynho32@gmail.com>
2020-12-19 19:48:58 -0300

    feat: Incluindo feature para listar itens de uma venda a partir do código do produto

--- src/app/controllers/Sales/Itens/ItensList.js
+++ src/app/controllers/Sales/Itens/ItensList.js
@@ -21,6 +21,19 @@ class SearchSale {
 
     return res.json(itens);
   }
+
+  async index(req, res) {
+    const { sale_id, product_id } = req.params;
+
+    const itens = await Item.findAll({
+      where: {
+        sale_id,
+        product_id,
+      },
+    });
+
+    return res.json(itens);
+  }
 }
 
 export default new SearchSale();
